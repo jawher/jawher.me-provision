ef9433d01142a5c16ab8ca0c6da2217093b9ef17
jawher <jawher.moussa@gmail.com>
2015-12-07 20:32:37 +0100

    New version of gorex

--- ansible/roles/gorex/files/gorex.js
+++ ansible/roles/gorex/files/gorex.js
@@ -4,25 +4,42 @@ angular.module('gorex', []);
 
 angular.module('gorex').factory('Rex', function($http){
 	return {
-		eval: function(re, input) {
+		eval: function(re, inputs) {
 			return $http.post('/eval', {
 				re: re,
-				input: input
+				inputs: inputs
 			});
 		}
 	};
 });
 
 angular.module('gorex').controller('RexController', function($scope, Rex){
+	$scope.inputs = [{v:''}];
+	
+	$scope.addInput = function() {
+		$scope.inputs.push({v:''});
+		$scope.rex();
+	};
+
+	$scope.delInput = function(index) {
+		$scope.inputs.splice(index, 1);
+		$scope.rex();
+	};
+
 	$scope.rex = function() {
-		Rex.eval($scope.re, $scope.input)
+		var inputs = []
+		for(var i=0;i<$scope.inputs.length; i++) {
+			inputs.push($scope.inputs[i].v)
+		}
+		Rex.eval($scope.re, inputs)
 			.success(function(data){
+				console.log(data)
 				delete $scope.error;
 				$scope.data=data;
 			})
 			.error(function(msg, code){
 				delete $scope.data;
 				$scope.error=msg;
-			})
+			});
 	}
 });
